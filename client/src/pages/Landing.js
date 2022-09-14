c9ea7a49e34405dfa7dc83bf201fa0d9836f5391
unknown <aumlan0120@gmail.com>
2022-09-14 23:41:40 +0600

    make logo component

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from '../assets/images/logo.svg';
+import Wrapper from '../assets/wrappers/LandingPage'
 import main from '../assets/images/main.svg';
 import styled from 'styled-components';
+import { Logo } from '../components';
 
 
 const Landing = () => {
   return <Wrapper>
     <nav>
-        <img src={logo} alt="jobify" className='logo'/>
+        <Logo/>
     </nav>
 
     <div className="container page">
@@ -28,44 +29,6 @@ const Landing = () => {
   </Wrapper>
 }
 
-
-const Wrapper =  styled.main`
-    nav{
-        width: var(--fluid-width);
-        max-width: var(--max-width);
-        margin: 0 auto;
-        height: var(--nav-height);
-        display: flex;
-        align-items: center;
-    }
-    .page{
-        mein-height: calc(100vh - var(--nav-height));
-        display: grid;
-        align-items: center;
-        margin-top: -3rem;
-    }
-    h1{
-        font-weight:700;
-        span{
-            color: var(--primary-500);
-        }
-    }
-    p{
-        color: var(--grey-500); 
-    }
-    .main-img{
-        display:none;
-    }
-    @media (min-width:992px){
-        .page{
-            grid-template-columns: 1fr 1fr;
-            column-gap: 3rem;
-        }
-        .main-img{
-        display:block;
-        }
-    }
-`
 
 
 export default Landing;
